008abef3ee35763f47f5bf54fa2adb90849d0c87
yanadyukova <yana.a.dukova@gmail.com>
2018-02-12 15:45:30 +0300

    add base structure

---
+++ src/Article.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export  default  function Article(props) {
+    const {article} = props
+    return (
+        <div>
+            <h3>{article.title}</h3>
+            <section>{article.text}</section>
+        </div>
+    )
+}
